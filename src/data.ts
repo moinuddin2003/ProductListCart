f7be1940c5839dcdc35ab80094707836c8c253b9
Muhammad Moinudddin <moinuddin010203@gmail.com>
2026-08-30 23:30:23 +0500

    Fixed URL for images and Live on gh pages

--- src/data.ts
+++ src/data.ts
@@ -4,7 +4,7 @@ export const data = [
             "thumbnail": "/images/image-waffle-thumbnail.jpg",
             "mobile": "/images/image-waffle-mobile.jpg",
             "tablet": "/images/image-waffle-tablet.jpg",
-            "desktop": "/images/image-waffle-desktop.jpg"
+            "desktop": "images/image-waffle-desktop.jpg"
        },
        "name": "Waffle with Berries",
        "category": "Waffle",
@@ -15,7 +15,7 @@ export const data = [
             "thumbnail": "/images/image-creme-brulee-thumbnail.jpg",
             "mobile": "/images/image-creme-brulee-mobile.jpg",
             "tablet": "/images/image-creme-brulee-tablet.jpg",
-            "desktop": "/images/image-creme-brulee-desktop.jpg"
+            "desktop": "images/image-creme-brulee-desktop.jpg"
         },
         "name": "Vanilla Bean Crème Brûlée",
         "category": "Crème Brûlée",
@@ -26,7 +26,7 @@ export const data = [
             "thumbnail": "/images/image-macaron-thumbnail.jpg",
             "mobile": "/images/image-macaron-mobile.jpg",
             "tablet": "/images/image-macaron-tablet.jpg",
-            "desktop": "/images/image-macaron-desktop.jpg"
+            "desktop": "images/image-macaron-desktop.jpg"
         },
         "name": "Macaron Mix of Five",
         "category": "Macaron",
@@ -37,7 +37,7 @@ export const data = [
             "thumbnail": "./images/image-tiramisu-thumbnail.jpg",
             "mobile": "/images/image-tiramisu-mobile.jpg",
             "tablet": "/images/image-tiramisu-tablet.jpg",
-            "desktop": "/images/image-tiramisu-desktop.jpg"
+            "desktop": "images/image-tiramisu-desktop.jpg"
         },
         "name": "Classic Tiramisu",
         "category": "Tiramisu",
@@ -48,7 +48,7 @@ export const data = [
             "thumbnail": "/images/image-baklava-thumbnail.jpg",
             "mobile": "/images/image-baklava-mobile.jpg",
             "tablet": "/images/image-baklava-tablet.jpg",
-            "desktop": "/images/image-baklava-desktop.jpg"
+            "desktop": "images/image-baklava-desktop.jpg"
         },
         "name": "Pistachio Baklava",
         "category": "Baklava",
@@ -59,7 +59,7 @@ export const data = [
             "thumbnail": "/images/image-meringue-thumbnail.jpg",
             "mobile": "/images/image-meringue-mobile.jpg",
             "tablet": "/images/image-meringue-tablet.jpg",
-            "desktop": "/images/image-meringue-desktop.jpg"
+            "desktop": "images/image-meringue-desktop.jpg"
         },
         "name": "Lemon Meringue Pie",
         "category": "Pie",
@@ -70,7 +70,7 @@ export const data = [
             "thumbnail": "/images/image-cake-thumbnail.jpg",
             "mobile": "/images/image-cake-mobile.jpg",
             "tablet": "/images/image-cake-tablet.jpg",
-            "desktop": "/images/image-cake-desktop.jpg"
+            "desktop": "images/image-cake-desktop.jpg"
         },
         "name": "Red Velvet Cake",
         "category": "Cake",
@@ -81,7 +81,7 @@ export const data = [
             "thumbnail": "/images/image-brownie-thumbnail.jpg",
             "mobile": "/images/image-brownie-mobile.jpg",
             "tablet": "/images/image-brownie-tablet.jpg",
-            "desktop": "/images/image-brownie-desktop.jpg"
+            "desktop": "images/image-brownie-desktop.jpg"
         },
         "name": "Salted Caramel Brownie",
         "category": "Brownie",
@@ -92,7 +92,7 @@ export const data = [
             "thumbnail": "/images/image-panna-cotta-thumbnail.jpg",
             "mobile": "/images/image-panna-cotta-mobile.jpg",
             "tablet": "/images/image-panna-cotta-tablet.jpg",
-            "desktop": "/images/image-panna-cotta-desktop.jpg"
+            "desktop": "images/image-panna-cotta-desktop.jpg"
         },
         "name": "Vanilla Panna Cotta",
         "category": "Panna Cotta",
